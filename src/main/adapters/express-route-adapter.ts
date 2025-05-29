bb556c20d4fc785e91ae24f070e3db93e34f7998
RP Neto <robertoperesneto@gmail.com>
2025-05-28 22:15:15 -0300

    refactor: reorganize import statements and improve accessToken handling in adaptRoute function

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -1,14 +1,16 @@
-import { type Controller } from '../../presentation/protocols'
 import { type Request, type Response } from 'express'
+import { type Controller } from '../../presentation/protocols'
 
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const request = {
       ...(req.body || {}),
       ...(req.params || {}),
-      ...(req.query || {}),
-      accessToken: req.headers?.authorization?.split(' ')[1] || ''
+      ...(req.query || {})
+      // accessToken: req.headers?.authorization?.split(' ')[1] || ''
     }
+    const token = req.headers?.authorization?.split(' ')[1]
+    if (token) request.accessToken = token
 
     const httpResponse = await controller.handle(request)
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
